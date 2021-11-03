634d88482b9992c301b2110fa9db54546ad2e381
Paulson Hanel <Paulson.Hanel@mymhc.ca>
2021-11-02 21:34:22 -0600

    visual studio files added

---
+++ LambSauseProgramV2/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿
+namespace LambSauseProgramV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 116);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ LambSauseProgramV2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//add this so I can make unhappy windows sounds
+using System.Media;
+//create timers in code so I don't have to do any form stuff
+using System.Timers;
+//NuGet package for tampering with windows audio
+using AudioSwitcher;
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace LambSauseProgramV2
+{
+    public partial class Form1 : Form
+    {
+        //define user controls
+        Form testnew;
+        //intelisense made these for me
+        private int _ScreenWidth;
+        private int _ScreenHeight;
+        Random rand;
+        Point mypoint;
+        SoundPlayer musicbox;
+        private static System.Timers.Timer aTimer;
+        private static System.Timers.Timer aTimer2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            musicbox = new SoundPlayer(LambSauseProgramV2.Properties.Resources.laumb_sausse_);
+
+            musicbox.Play();
+            //can I hide my form??? (neither of these work and I am not sure why! (NOTE: ask Derek)
+            this.Hide();
+            this.Visible = false;
+
+            //timers (some code from MS Online Docs)
+            aTimer = new System.Timers.Timer(2100);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += ATimer_Elapsed;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+
+            //timers (some code from MS Online Docs)
+            aTimer2 = new System.Timers.Timer(25);
+            // Hook up the Elapsed event for the timer. 
+            aTimer2.Elapsed += ATimer2_Elapsed;
+            aTimer2.AutoReset = true;
+            aTimer2.Enabled = true;
+
+            //instanciate and assign some stuff
+            rand = new Random();
+
+        }
+
+        private void ATimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //set windows voliume to max (like 95)
+            CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+            defaultPlaybackDevice.Volume = 95;
+            //play the sound
+            musicbox.Play();
+        }
+
+        private void ATimer2_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //instanciat the new form
+            testnew = new Form();
+
+            //put at begining to make sure it is executed
+            testnew.ShowInTaskbar = false;
+
+            //grabbing these for the constaints of the screen. This way it is better than guissing wher to place boxes
+            _ScreenWidth = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+            _ScreenHeight = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+            //System.Windows.Forms.Screen.AllScreens[1];
+            //figure out how to use        ^^^ to put them on both screens
+
+            //call this here becasue I want it to change every time
+            mypoint.X = rand.Next(_ScreenWidth);
+            mypoint.Y = rand.Next(_ScreenHeight);
+
+            //took this stuff from example in stack overflow (weirdly I remember previous try to work)
+            //https://stackoverflow.com/questions/24016638/set-form-location-c-sharp/24016834
+            testnew.StartPosition = FormStartPosition.Manual;
+            testnew.Left = mypoint.X;
+            testnew.Top = mypoint.Y;
+
+            //change the properties of the new form (we want it as inconveinent as possible)
+            testnew.FormBorderStyle = FormBorderStyle.None;
+            testnew.Width = 150;
+            testnew.Height = 80;
+
+            //change its image
+            testnew.BackgroundImage = LambSauseProgramV2.Properties.Resources.gordonRamsayM;
+            //testnew.BackgroundImageLayout = ImageLayout.Center;
+
+            testnew.Show();
+        }
+    }
+}
